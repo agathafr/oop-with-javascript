dca6216658d678701a10aea0afd8d7bd1d029d0f
Agatha França <agathafranca@outlook.com>
2022-05-14 12:07:23 -0300

    Exemplifies the definition of behavior for class

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
 class Customer {
   name;
   cpf;
+}
+
+class CurrentAccount {
   agency;
   balance;
+
+  withdrawal(money) {
+    if (this.balance >= money) {
+      this.balance -= money;
+    }
+  }
 }
 
 const customer1 = new Customer();
-const customer2 = new Customer();
-
 customer1.name = "Ricardo";
 customer1.cpf = 11122233309;
-customer1.agency = 1001;
-customer1.balance = 0;
 
+const customer2 = new Customer();
 customer2.name = "Alice";
 customer2.cpf = 88822233309;
-customer2.agency = 1001;
-customer2.balance = 0;
 
-console.log(customer1, customer2);
+const currentAccountRicardo = new CurrentAccount();
+
+currentAccountRicardo.balance = 0;
+currentAccountRicardo.agency = 1001;
+
+console.log(currentAccountRicardo.balance);
+
+currentAccountRicardo.balance += 100;
+
+console.log(currentAccountRicardo.balance);
+currentAccountRicardo.withdrawal(200);
+
+console.log(currentAccountRicardo.balance);
+
+console.log(customer1);
+console.log(customer2);
 
 //ctrl + alt  changes multiple lines at once
