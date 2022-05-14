bae859908737db8898f4e170799e0fb23cf7a99e
Agatha França <agathafranca@outlook.com>
2022-05-14 16:36:01 -0300

    Separates classes into files

---
+++ CurrentAccount.js
@@ -0,0 +1,17 @@
+class CurrentAccount {
+  agency;
+  //# https://github.com/tc39/proposal-class-fields#private-fields
+  _balance = 0;
+
+  withdrawal(money) {
+    if (this._balance >= money) {
+      this._balance -= money;
+      return money;
+    }
+  }
+
+  deposit(money) {
+    if (money <= 0) return;
+    this._balance += money;
+  }
+}
--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-class Customer {
-  name;
-  cpf;
-}
-
-class CurrentAccount {
-  agency;
-  //# https://github.com/tc39/proposal-class-fields#private-fields
-  _balance = 0;
-
-  withdrawal(money) {
-    if (this._balance >= money) {
-      this._balance -= money;
-      return money;
-    }
-  }
-
-  deposit(money) {
-    if (money <= 0) return;
-    this._balance += money;
-  }
-}
-
 const customer1 = new Customer();
 customer1.name = "Ricardo";
 customer1.cpf = 11122233309;
