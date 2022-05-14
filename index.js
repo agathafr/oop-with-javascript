3143d510ae4a7e723986792d1976354caabbfd30
Agatha França <agathafranca@outlook.com>
2022-05-14 11:08:53 -0300

    Class, attribute, instance/object and new operator

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 class Customer {
-  nome;
+  name;
   cpf;
-  agencia;
-  saldo;
+  agency;
+  balance;
 }
 
 const customer1 = new Customer();
+const customer2 = new Customer();
 
-const customer1Name = "Ricardo";
-const customer1CPF = 11122233309;
-const customer1Agency = 1001;
-const customer1Balance = 0;
+customer1.name = "Ricardo";
+customer1.cpf = 11122233309;
+customer1.agency = 1001;
+customer1.balance = 0;
+
+customer2.name = "Alice";
+customer2.cpf = 88822233309;
+customer2.agency = 1001;
+customer2.balance = 0;
+
+console.log(customer1, customer2);
 
 //ctrl + alt  changes multiple lines at once
-const customer2Name = "Alice";
-const customer2CPF = 88822233309;
-const customer2Agency = 1001;
-const customer2Balance = 0;
-
-console.log(customer1Name);
