1012a7a11b1a6ecb91fb6b15deb5dd62f14941e0
Agatha França <agathafranca@outlook.com>
2022-05-14 10:51:24 -0300

    Exemplifies code repetition

---
+++ index.js
@@ -0,0 +1,12 @@
+const customer1Name = "Ricardo";
+const customer1CPF = 11122233309;
+const customer1Agency = 1001;
+const customer1Balance = 0;
+
+//ctrl + alt  changes multiple lines at once
+const customer2Name = "Alice";
+const customer2CPF = 88822233309;
+const customer2Agency = 1001;
+const customer2Balance = 0;
+
+console.log(customer1Name);
