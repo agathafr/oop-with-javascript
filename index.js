3e3d0ccb19dc9f18f66f1af6b2831ac6d5b6bd54
Agatha França <agathafranca@outlook.com>
2022-05-18 09:06:51 -0300

    Deletes unusable code

--- index.js
+++ index.js
@@ -1,17 +1,2 @@
 import { Customer } from "./Customer.js";
-import { CurrentAccount } from "./Account/CurrentAccount.js";
-import { SavingsAccount } from "./Account/SavingsAccount.js";
-import { SalaryAccount } from "./Account/SalaryAccount.js";
 
-const customer = new Customer("Alice", 88822233309);
-
-const currentAccountAlice = new CurrentAccount(customer, 1001);
-const savingsAccount = new SavingsAccount(700, customer, 1001);
-const salaryAccount = new SalaryAccount(customer);
-
-salaryAccount.deposit(100);
-salaryAccount.withdrawal(10);
-
-console.log(salaryAccount);
-
-//ctrl + alt  changes multiple lines at once
