dbf08e407849a29c281bc0b5e34f1de94483d991
Agatha França <agathafranca@outlook.com>
2022-05-18 10:47:58 -0300

    Creates the logic to validate the existence of the property and method

--- AuthenticationSystem.js
+++ AuthenticationSystem.js
@@ -1,8 +1,16 @@
 /*
  To be authenticable is to have the authenticate method
+ duck typing
 */
 export class AuthenticationSystem {
   static login(authenticable, password) {
-    return authenticable.authenticate(password);
+    if(AuthenticationSystem.isAuthenticable(authenticable)) {
+      return authenticable.authenticate(password);
+    }
+    return false;
+  }
+
+  static isAuthenticable(authenticable) {
+    return "authenticate" in authenticable && authenticable.authenticate instanceof Function;
   }
 }
