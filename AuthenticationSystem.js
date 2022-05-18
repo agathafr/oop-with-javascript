7028e4f053e97bd62364d3e773401fbcde8a3567
Agatha França <agathafranca@outlook.com>
2022-05-18 09:53:27 -0300

    Exemplifies interface

--- AuthenticationSystem.js
+++ AuthenticationSystem.js
@@ -1,5 +1,8 @@
+/*
+ To be authenticable is to have the authenticate method
+*/
 export class AuthenticationSystem {
-  static login(employee, password) {
-    return employee.password == password;
+  static login(authenticable, password) {
+    return authenticable.authenticate(password);
   }
 }
--- index.js
+++ index.js
@@ -4,11 +4,15 @@ import { Director } from "./Employee/Director.js";
 import { AuthenticationSystem } from "./AuthenticationSystem.js";
 
 const director = new Manager("Rodrigo", 10000, 12345678900);
-director.registerPassword("123456789")
+director.registerPassword("123456")
 
 const manager = new Manager("Ricardo", 5000, 12345678901);
-manager.registerPassword("123456")
+manager.registerPassword("123")
 
-const areLoggedIn = AuthenticationSystem.login(manager, "123456");
+const customer = new Customer("Lais", 78945612379, "456")
 
-console.log(areLoggedIn);
+const directorIsLoggedIn = AuthenticationSystem.login(director, "123456");
+const managerIsLoggedIn = AuthenticationSystem.login(manager, "123");
+const customerIsLoggedIn = AuthenticationSystem.login(customer, "456");
+
+console.log(directorIsLoggedIn, managerIsLoggedIn, customerIsLoggedIn);
